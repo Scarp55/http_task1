ceb87e2022bb95eee0d10d386678807a7b0c7b58
Scarp55 <120288@mail.ru>
2022-09-20 22:24:13 +0600

    first

---
+++ src/main/java/http_task1/FactsOfCats.java
@@ -0,0 +1,52 @@
+package http_task1;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class FactsOfCats {
+
+	String id;
+	String text;
+	String type;
+	String user;
+	String upvotes;
+
+	public FactsOfCats() {
+	}
+
+	public FactsOfCats(@JsonProperty("id") String id, @JsonProperty("text") String text,
+			@JsonProperty("type") String type, @JsonProperty("user") String user,
+			@JsonProperty("upvotes") String upvotes) {
+		this.id = id;
+		this.text = text;
+		this.type = type;
+		this.user = user;
+		this.upvotes = upvotes;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public String getUpvotes() {
+		return upvotes;
+	}
+
+	@Override
+	public String toString() {
+		return "FactAboutCat \n{\n" + "id = '" + id + '\'' + ",\n " + "text = '" + text + '\'' + ",\n " + "type = '"
+				+ type + '\'' + ",\n " + "user = '" + user + '\'' + ",\n " + "upvotes = " + upvotes + "\n}\n";
+	}
+
+}
